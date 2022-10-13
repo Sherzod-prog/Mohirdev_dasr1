bac0ddf065d6e9ced641620b980eba4cd3424fb5
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-13 07:50:44 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// "use strict"
+
+// const number = 10;
+// let myName = 'Sherzod';
+// console.log(myName)
+// console.log(number)
+
+const age = +prompt('asfklsdkf');
+console.log(age)
+console.log();

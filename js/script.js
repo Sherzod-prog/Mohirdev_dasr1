2be7310badc4fe51fcfb3a6362f235f6c7f2bfaf
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-13 22:30:10 +0500

    vazifa1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-// "use strict"
+"use strict"
 
 // const number = 10;
 // let myName = 'Sherzod';
 // console.log(myName)
 // console.log(number)
 
-const age = +prompt('asfklsdkf');
-console.log(age)
-console.log();
+// const age = +prompt('asfklsdkf');
+// console.log(age)
+// console.log();
+
+//  let startNumber = 1
+// while (startNumber <= 30) {
+//     console.log(startNumber);
+//     startNumber ++
+// }
+
+let num = 12
+for (let i = 1; i <= 8; i++ ) {
+    console.log(i);
+}

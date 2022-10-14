dae906a0a9a0b5631380da279c95a6ae7995b1e4
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-14 07:48:50 +0500

    functions

---
+++ js/funtionss.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function sayHelloWorld () {
+    console.log("Hello world!");
+}
+
+sayHelloWorld()
+
+const user = "Sherzod"
+
+console.log(user.toUpperCase());
+
+const greeting = "Hello world"
+console.log(greeting.slice(-3, 0,));
+
+
+const border = "dd1"
+console.log(parseInt(border));

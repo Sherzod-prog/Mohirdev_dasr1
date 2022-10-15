af478723b5e0e3f677dbd860898ca353c2197f75
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-15 07:53:07 +0500

    davomi

--- vazifa1.js
+++ vazifa1.js
@@ -1,6 +1,21 @@
 'use strict'
 
-const numberOfSeries = +prompt("Nechta serial ko'rdingiz?", '')
+let numberOfSeries
+
+function startApp(){
+   numberOfSeries = +prompt("Nechta serial ko'rdingiz?", '')
+
+   while (
+    numberOfSeries == "" ||
+    numberOfSeries == null ||
+    isNaN(numberOfSeries)
+   )
+   {
+    numberOfSeries = +prompt("Nechta serial ko'rdingiz?", '')
+   }
+}
+
+startApp();
 
 const seriesDB = {
   count: numberOfSeries,
@@ -10,27 +25,50 @@ const seriesDB = {
   private: false,
 }
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Oxirgi ko'rgan serialingiz?"),
-    b = prompt('Nechi baxo berasiz?')
-
-  if (a != null && b != null && a != '' && b != '') {
-    seriesDB.series[a] = b
-    console.log('done')
-  } else {
-    console.log('error')
-    i--
+function rememberMySeries() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Oxirgi ko'rgan serialingiz?"),
+      b = prompt('Nechi baxo berasiz?')
+  
+    if (a != null && b != null && a != '' && b != '') {
+      seriesDB.series[a] = b
+      console.log('done')
+    } else {
+      console.log('error')
+      i--
+    }
   }
 }
 
-if (seriesDB.count < 5) {
-  console.log('Kam serial ko’ripsiz')
-} else if (seriesDB.count >= 5 && seriesDB.count < 10) {
-  console.log('Siz classik tamoshabin ekansiz')
-} else if (seriesDB.count >= 10) {
-  console.log('Siz serialchi zvezda ekansiz')
-} else {
-  console.log('Error')
+rememberMySeries();
+
+function detectLevelSeries () {
+  if (seriesDB.count < 5) {
+    console.log('Kam serial ko’ripsiz')
+  } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+    console.log('Siz classik tamoshabin ekansiz')
+  } else if (seriesDB.count >= 10) {
+    console.log('Siz serialchi zvezda ekansiz')
+  } else {
+    console.log('Error')
+  }
 }
 
-console.log(seriesDB)
+detectLevelSeries();
+
+function showDB(hidden){
+  if (!hidden){
+    console.log(seriesDB)
+  }
+}
+
+showDB(seriesDB.private);
+
+function writeGenres (){
+  for (let i = 0; i <= 2; i++){
+    const genre = prompt(`Yaxsh ko'rgan janringiz ${i + 1}`);
+    seriesDB.genres[i] = genre
+  }  
+}
+
+writeGenres();

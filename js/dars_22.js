e43330ff3c801507bf287cbe7391abf57cdf66ab
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-16 07:35:32 +0500

    davomi4

---
+++ js/dars_22.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const number = {
+    x: 10,
+    y: 5
+};
+
+const addNumbers = {
+    z: 18,
+    w: {
+        a: 22,
+        b: 14
+    }
+};
+
+// const allNumbers = Object.assign(number, addNumbers)
+
+// allNumbers.w.a = 44
+// addNumbers.w.a = 50
+
+console.log(number);
+console.log(addNumbers);
+// console.log(allNumbers);

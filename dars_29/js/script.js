2c1c222856cbfbbc8c599e01fc8a3ccdd8ebabb0
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-18 07:41:42 +0500

    Dars30

--- dars_29/js/script.js
+++ dars_29/js/script.js
@@ -1,5 +1,8 @@
 'use strict'
 
+document.addEventListener('DOMContentLoaded', () => {
+
+
 // 1. Reklama blokini o'chirish
 // 2. Drama janrini komediyaga o'zgartiring
 // 3. Orqa fonni o'zgartiring. JS yordamida
@@ -10,23 +13,41 @@
 const adv = document.querySelectorAll('.promo__adv img'),
   wrapper = document.querySelector('.promo__bg'),
   genre = wrapper.querySelector('.promo__genre'),
-  seriesList = document.querySelector('.promo__interactive-list')
+  seriesList = document.querySelector('.promo__interactive-list'),
+  addForm = document.querySelector('form.add'),
+  inpitVal = document.querySelector('.adding__input'),
+  checkbox = document.querySelector("[type='checkbox']")
 
 
 
-  // 1. Reklama blokini o'chirish
+  // 32 dars 1. funcsion yozing submit bosilganda sahifa qayta yuklanmasligi kerak.
+  // va inputga yozilganda serialni seriesDB.series ga massivga qo'shing
+  // input ichidagi qiymatni olish uchun input.value yordamida oling
+   
+  // 32 dars 2. Agar serialni nomi 18 tadan ko'p bo'lsa uni kesib ... bilan yozing
+
+  // 32 dars 3. Agar formada checkbox done belgisi qo'yilsa console ga "Sevimli serial qo'shildi" xabarini chiqaring
+
+  // 32 dars 4. Hamma serial nomlari alfavit ket ma ketlikda bo'lsin
+
+  // 32 dars IXTYORIY axlat qutisi bosilganda o'sha serial o'chirilsin
+
+
+
+
+  // 29 dars 1. Reklama blokini o'chirish 
   adv.forEach((item) => {
     item.remove()
   })
 
-  // 2. Drama janrini komediyaga o'zgartiring
+  // 29 dars 2. Drama janrini komediyaga o'zgartiring
 genre.textContent = 'comedy'
 
-  // 3. Orqa fonni o'zgartiring. JS yordamida
+  // 29 dars 3. Orqa fonni o'zgartiring. JS yordamida
   wrapper.style.backgroundImage = 'url("img/1.jpg")'
 
-  // 4. Seriallarni JS dagi massiv yordamida ko'rsating
-  // 5. Har bir serialni raqami bo'lsin.
+  // 29 dars 4. Seriallarni JS dagi massiv yordamida ko'rsating
+  // 29 dars 5. Har bir serialni raqami bo'lsin.
   
 const seriesDB = {
   series: [
@@ -47,6 +68,9 @@ seriesDB.series.forEach((item, ind) =>{
        <div class="delete"></div>
      </li>
   `
+
+
+
 })
 
 
@@ -117,3 +141,4 @@ seriesDB.series.forEach((item, ind) =>{
 //     </li>
 //   `
 // })
+})
---
+++ dars_30/js/script.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// 29 dars boshlanishi
+
+// const btn = document.querySelector('#btn')
+
+// // btn.addEventListener('click', () => {
+// //     alert('click')
+// // })
+
+// // btn.addEventListener('click', () => {
+// //     alert('click 2')
+// // })
+
+// // btn.addEventListener('mouseenter', (event) => {
+// //     console.log(event.target);
+// // })
+
+// let i = 0
+// const addElement = (event) => {
+//     i++
+//     if (i == 3 ){
+//         btn.removeEventListener('click', addElement)
+//     }
+//     console.log(i);
+// }
+
+// btn.addEventListener('click', addElement)
+
+
+// const btns = document.querySelectorAll('button')
+
+// btns.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         console.log(1);
+//     })
+// })
+
+
+// 29 dars tugashi
+
+// 30 dars boshlanishi
+
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('[data-current="b"]').previousSibling);
+// console.log(document.querySelector('[data-current="b"]').nextSibling);
+// console.log(document.querySelector('[data-current="b"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="b"]').previousElementSibling);
+
+
+
+// text elementlari chiqarmaslik
+
+// for (let node of document.body.childNodes){
+//     if(node.nodeName == '#text' ){
+//         continue
+//     }    
+//     console.log(node);
+// }
+
+
+// 30 dars tugashi

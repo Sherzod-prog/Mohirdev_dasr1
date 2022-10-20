915345de351d50654b27f5e749f44ec0598d3a29
Sherzod-prog <65225425+Sherzod-prog@users.noreply.github.com>
2022-10-20 07:42:36 +0500

    Dars37 davomi

--- dars_40/js/script.js
+++ dars_40/js/script.js
@@ -2,20 +2,23 @@
 
 // const now = new Date()
 
-// console.log(now.getDate());
+// console.log(now);
 // console.log(now.getDay());
 // console.log(now.getHours());
 // console.log(now.getTime());
 // console.log(now.getSeconds());
 
 
-let start = new Date()
+// let start = new Date()
 
-for(let i = 0; i <  1000000; i++){
-     let comp = i ** 3
-     comp
-}
+// for(let i = 0; i <  1000000; i++){
+//      let comp = i ** 3
+//      comp
+// }
 
-let end = new Date()
+// let end = new Date()
 
-alert(`asas ${end - start}`);
+// alert(`asas ${end - start}`);
+
+const deadline = '2022:11:23'
+     console.log(deadline);
